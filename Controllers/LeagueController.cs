f9b17e37fade8c322298cb7afcf1b99cd5db59ef
Andrew Bumpus <andrewpbumpus@gmail.com>
2022-04-26 11:33:15 -0400

    Add start and end date to leagues

--- Controllers/LeagueController.cs
+++ Controllers/LeagueController.cs
@@ -1,20 +1,24 @@
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.AspNetCore.Mvc;
+using golf_leagues_identity.Services;
 
 namespace golf_leagues_identity.Controllers;
 
 public class LeagueController : Controller
 {
     private readonly ILogger<LeagueController> _logger;
+    private readonly ILeagueService _leagueService;
 
-    public LeagueController(ILogger<LeagueController> logger)
+    public LeagueController(ILogger<LeagueController> logger, ILeagueService leagueService)
     {
         _logger = logger;
+        _leagueService = leagueService;
+
     }
 
     [HttpGet("leagues/all")]
-    public IActionResult GetAll([FromRoute]string clientId)
+    public IActionResult GetAll()
     {
-        return Ok();
+        return Ok(_leagueService.GetAll());
     }
 }
---
+++ Migrations/20220426150223_AddStartAndEndDateToLeagues.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace golf_leagues_identity.Migrations
+{
+    public partial class AddStartAndEndDateToLeagues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                schema: "golf_league",
+                table: "League",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                schema: "golf_league",
+                table: "League",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                schema: "golf_league",
+                table: "League");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                schema: "golf_league",
+                table: "League");
+        }
+    }
+}
