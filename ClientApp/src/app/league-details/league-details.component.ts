11239472fd0db4719ec6401ab94c7776f53a9b01
Andrew Bumpus <andrewpbumpus@gmail.com>
2022-05-02 10:28:59 -0400

    Add players table to league details

--- ClientApp/src/app/league-details/league-details.component.ts
+++ ClientApp/src/app/league-details/league-details.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import * as moment from 'moment';
 import { League } from '../models/league.model';
+import { Player } from '../models/player.model';
 import { PlayerCreatorComponent } from '../player-creator/player-creator.component';
 
 @Component({
@@ -12,12 +14,15 @@ import { PlayerCreatorComponent } from '../player-creator/player-creator.compone
 })
 export class LeagueDetailsComponent implements OnInit {
   public league: League;
+  public displayedColumns: string[] = ['name', 'handicap', 'points'];
+  public players: MatTableDataSource<Player>;
   public moment = moment;
   constructor(
     private route: ActivatedRoute,
     public dialog: MatDialog
   ) { 
     this.league = this.route.snapshot.data['league'];
+    this.players = new MatTableDataSource<Player>(this.league.players);
     console.log(this.league);
   }
 
